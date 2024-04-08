a07d09bb7e581dba6bf471d771f28a7ffae351e5
Ubuntu <Panda@mz-vm-dev.ztb5m0p3nhvufk2uqxaqnevjrh.ix.internal.cloudapp.net>
2024-04-08 18:36:14 +0000

    added ability for randomly choose emoji from img folder

--- website/pages.py
+++ website/pages.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, url_for
 import yaml
+import os
+import random
 
 pages = Blueprint('pages', __name__)
 
@@ -11,13 +13,16 @@ with open(DATA_FILE, 'r') as f:
 
 @pages.route('/<http_code>')
 def root(http_code):
-    
+    emojis = os.listdir('website/static/img')
+    emoji = random.choice(emojis)
+    emoji_path = url_for('static',filename=f'img/{emoji}.png')
     error_page = next((item for item in datas if item['code'] == http_code), None)
     
     if error_page is None:
         return render_template('base.html', 
                                 title='test', 
                                 level='4',
+                                emoji_path=emoji_path,
                                 code='4',
                                 header_two='test', 
                                 paragraph='test')
@@ -25,6 +30,7 @@ def root(http_code):
     return render_template('base.html', 
                             title=error_page['title'], 
                             level=error_page['code'][0],
+                            emoji_path=emoji_path,
                             code=error_page['code'][-1],
                             header_two=error_page['h2'], 
                             paragraph=error_page['paragraph'])
